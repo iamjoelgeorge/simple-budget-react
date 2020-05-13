1cc89384f5e9c442d2150022acb3c4559abf2aea
Joel George <mynameisjoelgeorge@gmail.com>
2020-05-13 13:44:14 +0530

    Updates

---
+++ precache-manifest.697a2cbae211cf53f0ac003902b6f1eb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0934fa2b4b4ed49bcd9b3fe31a653d91",
+    "url": "/simple-budget-react/index.html"
+  },
+  {
+    "revision": "556b90e08b2e673b4d03",
+    "url": "/simple-budget-react/static/css/main.4ad5a732.chunk.css"
+  },
+  {
+    "revision": "e3d21fb959e261ab869e",
+    "url": "/simple-budget-react/static/js/2.2b102b5b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/simple-budget-react/static/js/2.2b102b5b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "556b90e08b2e673b4d03",
+    "url": "/simple-budget-react/static/js/main.276f2c3c.chunk.js"
+  },
+  {
+    "revision": "0da3a41a791b083e21e6",
+    "url": "/simple-budget-react/static/js/runtime-main.9faa699e.js"
+  }
+]);
