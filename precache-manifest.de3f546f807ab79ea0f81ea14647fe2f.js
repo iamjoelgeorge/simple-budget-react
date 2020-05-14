d2f35433294a2e1eb08bf4a528492c99bf6226b5
Joel George <mynameisjoelgeorge@gmail.com>
2020-05-14 16:03:12 +0530

    Updates

--- precache-manifest.de3f546f807ab79ea0f81ea14647fe2f.js
+++ precache-manifest.de3f546f807ab79ea0f81ea14647fe2f.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "fc5bb76e20cf724f42df867c54b3c5f8",
+    "revision": "8e04812d73bfa1fcae961bb77cd99559",
     "url": "/simple-budget-react/index.html"
   },
   {
-    "revision": "0e4002b19b92552a14ca",
+    "revision": "727e1192a32a62fde8bb",
     "url": "/simple-budget-react/static/css/main.782b1046.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/simple-budget-react/static/js/2.48eb501b.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "0e4002b19b92552a14ca",
-    "url": "/simple-budget-react/static/js/main.2a2d256c.chunk.js"
+    "revision": "727e1192a32a62fde8bb",
+    "url": "/simple-budget-react/static/js/main.648d76fc.chunk.js"
   },
   {
     "revision": "0da3a41a791b083e21e6",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/simple-budget-react/precache-manifest.0a76163c9cf53461b74b56a41694e54e.js"
+  "/simple-budget-react/precache-manifest.de3f546f807ab79ea0f81ea14647fe2f.js"
 );
 
 self.addEventListener('message', (event) => {
